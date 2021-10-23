4a425796c8d1021e49c9c097b49314c9a3c175b2
I-AzraeL-I <qaraqan70@gmail.com>
2021-10-23 17:17:50 +0200

    initial commit

---
+++ src/main/java/com/mycompany/mszczepienia/MSzczepieniaApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.mszczepienia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSzczepieniaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MSzczepieniaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/mszczepienia/MSzczepieniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.mszczepienia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSzczepieniaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
