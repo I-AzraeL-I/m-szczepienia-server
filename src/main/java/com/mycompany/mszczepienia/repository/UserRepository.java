98c6e9fc97d5a3ce71fa48ca092c73f2f7dd079d
Bartek306 <shwdp13@wp.pl>
2021-12-12 15:13:45 +0100

    fix data loader

--- src/main/java/com/mycompany/mszczepienia/repository/UserRepository.java
+++ src/main/java/com/mycompany/mszczepienia/repository/UserRepository.java
@@ -4,12 +4,13 @@ import com.mycompany.mszczepienia.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
-
+    List<User> findAllByRole(String role);
     boolean existsByEmail(String email);
 }
